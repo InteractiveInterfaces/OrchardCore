347d5fac7bd9eb16cf9e4b3a818c148723f2bfa4
Mayur Bharodia <mayur@interactiveinterfaces.ca>
2020-07-02 12:32:31 -0600

    Adding required changes to add IntInt theme for Interactive corporate site

---
+++ src/OrchardCore.Themes/TheIntIntTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "TheIntIntTheme",
+    Author = "Interactive Interfaces Ltd",
+    Website = "http://www.interactiveinterfaces.ca",
+    Version = "1.0.0",
+    Description = "Theme for corporate site of Interactive Interfaces Ltd"
+)]
